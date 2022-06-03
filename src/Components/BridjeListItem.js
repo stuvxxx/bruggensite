671ac217632846bb65cb76844eb83b3715c3d5fd
Marc Zwiers <mczwiers@gmail.com>
2022-06-03 15:35:22 +0200

    alles gecentered

--- src/Components/BridjeListItem.js
+++ src/Components/BridjeListItem.js
@@ -2,14 +2,14 @@ import React from "react";
 
 function BridgeListItem({name, loc, link}) {
     return (
-        <li className="list-item-container">
-            <h4>{name}</h4>
+        <div className="list-item-container">
+            <h3>{name}</h3>
             <div className="locatie">
-            <h3>Locatie</h3>
+            <h4>Locatie</h4>
             <p>{loc}</p>
             </div>
             <a href={link}>Foto</a>
-        </li>
+        </div>
 
     )
 }
