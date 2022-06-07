72a48d7239d42ab66345edfb29e8458d0fd7540f
Marc Zwiers <mczwiers@gmail.com>
2022-06-07 19:09:20 +0200

    lellekl!

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,9 @@
 import React from "react";
 
+
 function Header() {
     return (
         <div className="header-container">
-            <img url="brugklein.png" alt="burggetje"></img>
             <h1>Geen brug te ver!</h1>
         </div>
     )
