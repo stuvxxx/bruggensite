6a91b471e8d8fa08d04433929680e31846f81f86
Marc Zwiers <mczwiers@gmail.com>
2022-06-08 15:42:52 +0200

    Text toegevoegd!

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,31 @@
+
+
+
 import React from "react";
 
 
 function Header() {
     return (
         <div className="header-container">
-            <h1>Geen brug te ver</h1>
+            <div className="text-container">
+                <h1>WELKOM</h1>
+                <hr></hr>
+                <p>Amsterdam heeft meer dan 5000 bruggen.
+                   Das niet niks en het leek mij een goed idee hier eens wat meer aandacht aan te besteden.
+                   Vandaar deze website! 
+                   <br></br>
+                   Hier onder vindt je kaartjes van elke brug van Amsterdam. Heb je een brug bezocht, dan draai je het 
+                   kaartje om, beoordeel je de brug en voeg hem toe aan je collection. 
+                   <br></br>
+                   Wellicht koester je een mooie herinnering die je later terug uit je collectie kan halen!
+                   <br></br>
+                   Overigens is deze site deel van mijn portefolio, ik ben een 
+                   startende Front-End developer. 
+                   Voor meer info zie:contact hieronder!
+                   <br></br>
+                   Marc Zwiers
+                </p>
+            </div>
         </div>
     )
 }
