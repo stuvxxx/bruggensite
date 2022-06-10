b0cc614f3c991ed9bc9b5478566ef380a02a384b
Marc Zwiers <mczwiers@gmail.com>
2022-06-10 09:35:07 +0200

    sads

---
+++ src/Components/Rate.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Rate extends Component {
+  render() {
+    return (
+      <div className='rate'>
+          <h1>WAAR BEN JE?</h1>
+      </div>
+    )
+  }
+}
+
+export default Rate
--- src/Main.js
+++ src/Main.js
@@ -5,6 +5,7 @@ import BridgeList from "./Components/BridgeList";
 import Contact from "./Components/Contact";
 import Collection from "./Components/Collection";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Rate from "./Components/Rate";
 
 
 
@@ -16,6 +17,10 @@ function Main() {
         <Nav />
         <Routes>
           <Route path="/" element={<BridgeList />}/>
+            <Route path='/rate' element={<Rate />}>
+                  <Route path=":rateid" element={<Rate />}>
+            </Route>
+          </Route>
           <Route path="/collection" element={<Collection />}/>
           <Route path="/contact" element={<Contact />}/>
         </Routes>
